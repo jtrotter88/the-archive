125ed9a537ecdcaa2183affeac22510da5d4c202
jtrotter88 <lifeupneeded1up@gmail.com>
2018-02-15 20:38:47 -0600

    Output Test

---
+++ outputTest.js
@@ -0,0 +1,9 @@
+function consolePrint(){
+    console.log("I have a message for you in the console!");
+}
+function alertPrint(){
+    alert("I have a message for you in an alert!");
+}
+function documentPrint(){
+    document.write("I have a message for you on a document!");
+}
