17ef0f9a86b98f0acf4a04b6b0d1966b8634f965
jtrotter88 <lifeupneeded1up@gmail.com>
2018-04-10 19:43:05 -0500

    update

--- taskTrackerApp2/app.js
+++ taskTrackerApp2/app.js
@@ -20,7 +20,9 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
+//inform the app to use ejs as it's view engine
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
